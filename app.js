928695acd0d9e39bf84e47e5b07b322f49f4afb3
shivam1192 <sg9827252555@gmail.com>
2020-01-20 20:50:51 +0530

    message facility running

--- app.js
+++ app.js
@@ -11,8 +11,25 @@ var transporter = nodemailer.createTransport({
     auth:{
         user: 'sg9827252555@gmail.com',
         pass: process.env.PASSWORD
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 })
+
+const mailOptions={
+    from:'sg98272252555@gmail.com',
+    to: 'prajwal714singh@gmail.com',
+    subject:'Blockchain checking',
+    html: '<p>Hello world</p>'
+}
+transporter.sendMail(mailOptions,function(err,info){
+    if(err)
+    console.log(err)
+    else
+    console.log(info)
+})
+
 app.listen(3003,()=>{
     console.log("your server is started");
 })
